8cec458e15c725a8d4910e9ec5b15637701e749e
Zidane Innis <zidaneinnis@gmail.com>
2023-01-09 16:07:06 -0500

    Complete

--- src/components/User.js
+++ src/components/User.js
@@ -1,7 +1,18 @@
-import classes from './User.module.css';
+import { Component } from "react";
+import classes from "./User.module.css";
 
-const User = (props) => {
-  return <li className={classes.user}>{props.name}</li>;
-};
+class User extends Component {
+  componentWillUnmount() {
+    console.log("USER WILL UNMOUNT");
+  }
+
+  render() {
+    return <li className={classes.user}>{this.props.name}</li>;
+  }
+}
+
+// const User = (props) => {
+//   return <li className={classes.user}>{props.name}</li>;
+// };
 
 export default User;
